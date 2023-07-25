1b276edd0b14ab97bea331082fcbf95c9f406851
fmaliha <fmaliha.maliha9@gmail.com>
2023-07-25 15:04:29 +0600

    style : add input page

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import {
 import Home from "./pages/home/Home";
 import Single from "./pages/single/single";
 import List from "./pages/list/List";
+import { propertyInputs, userInputs } from "./formSource";
+import New from "./pages/new/New";
 
 function App() {
   return (
@@ -19,9 +21,23 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="single" element={<Single />} />
-            <Route path="/users">
+            <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add New User" />}
+              />
+            </Route>
+            <Route path="properties">
+              <Route index element={<List />} />
+              <Route path=":propertyId" element={<Single />} />
+              <Route
+                path="new"
+                element={
+                  <New inputs={propertyInputs} title="Add New Property" />
+                }
+              />
             </Route>
           </Route>
         </Routes>
---
+++ client/src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: "Username",
+    type: "text",
+    placeholder: "Atsuki_Watanabe",
+  },
+  {
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "Atsuki Watanabe",
+  },
+  {
+    id: 3,
+    label: "Email",
+    type: "mail",
+    placeholder: "watanabe@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Phone",
+    type: "text",
+    placeholder: "+1 234 567 89",
+  },
+  {
+    id: 5,
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "11-2/a, Adachi-ku,Tokyo",
+  },
+  {
+    id: 7,
+    label: "Country",
+    type: "text",
+    placeholder: "Tokyo",
+  },
+];
+
+export const propertyInputs = [
+  {
+    id: 1,
+    label: "Title",
+    type: "text",
+    placeholder: "Project Purbachal Dreams",
+  },
+  {
+    id: 2,
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  {
+    id: 3,
+    label: "Category",
+    type: "text",
+    placeholder: "Land",
+  },
+  {
+    id: 4,
+    label: "Price",
+    type: "text",
+    placeholder: "10000",
+  },
+  {
+    id: 5,
+    label: "Location",
+    type: "text",
+    placeholder: "Baridhara",
+  },
+];
